158ececb42a50d254ac40ad1f012a731571e485d
Alan Eicker <alaneicker@gmail.com>
2024-08-09 09:04:23 -0500

    integrates react-particles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,113 @@
-import '@/styles/globals.scss';
+import { useCallback } from 'react';
 import { usePathname } from 'next/navigation';
 import { AnimatePresence } from 'framer-motion';
+import Particles from 'react-particles';
+import { loadSlim } from 'tsparticles-slim';
 
 import Header from '@/components/Header';
 import content from '../static/content.yaml';
 
+import '@/styles/globals.scss';
+
 const { header } = content;
+
+const particlesOptions = {
+  background: {
+    color: {
+      value: '#222831',
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: false,
+      },
+      onHover: {
+        enable: false,
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.2,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#ffffff',
+    },
+    links: {
+      color: '#ffffff',
+      distance: 150,
+      enable: true,
+      opacity: 0.1,
+      width: 1,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outModes: {
+        default: 'bounce',
+      },
+      random: false,
+      speed: 1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.15,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+};
 
 export default function App({ Component, pageProps }) {
   const pathname = usePathname().replace('/', '');
   const section = pathname === '' ? 'home' : pathname;
 
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
     <>
-      <Header logo={header.logo} navItems={header.navItems} />
-      <main>
-        <AnimatePresence initial={false} mode="wait">
-          <Component key={section} {...pageProps} />
-        </AnimatePresence>
-      </main>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particlesOptions}
+      />
+      <div className="app">
+        <Header logo={header.logo} navItems={header.navItems} />
+        <main>
+          <AnimatePresence initial={false} mode="wait">
+            <Component key={section} {...pageProps} />
+          </AnimatePresence>
+        </main>
+      </div>
     </>
   );
 }
